f0c100f052ecd6acd80162043bc09f27f5fda333
Daniel Hernandez <dh893177@gmail.com>
2024-06-12 11:29:06 -0500

    done with exercise 2

--- scripts/edit-todo.js
+++ scripts/edit-todo.js
@@ -2,8 +2,7 @@
 
 window.onload = () =>{
 
-    let deleteSection = document.querySelector("#deleteId")
-    deleteSection.addEventListener("submit", deleteAComment )
+
 
     let getIdToEdit = document.querySelector("#getIdToEdit")
     getIdToEdit.addEventListener("submit", populateUpdateForm)
@@ -13,36 +12,6 @@ window.onload = () =>{
 
 }
 
-
-//method/function to delete a comment
-//CRUD: (D)elete a comment
-const deleteAComment = async (event) => {
-
-    event.preventDefault();
-
-    //try catch for error handling
-    try {
-
-        //make a fetch (DELETE) request to remove a comment in the API
-        let response = await fetch("https://jsonplaceholder.typicode.com/todos/" + event.target.todoId.value,
-            {
-                method: "DELETE"
-            }
-        );
-        //turn those comments in to something we can work with
-        let deletedId = await response.json();
-
-        //put the comments in the console
-        console.log(deletedId)
-
-    } catch (err) {
-
-        //what the hell happend
-        console.log("something went south")
-
-    }
-
-}
 
 //method/function to update a comment
 //CRUD: (U)pdate a comment
@@ -54,13 +23,12 @@ const updateAComment = async (event) => {
     try {
 
         //make a fetch (PUT) request to update a comment in the API
-        let response = await fetch("https://jsonplaceholder.typicode.com/todos/" + event.target.inputId.value,
+        let response = await fetch("https://jsonplaceholder.typicode.com/todos/" + event.target.id.value,
             {
                 method: "PUT",
                 headers: { "Content-type": "application/json; charset=UTF-8" },
                 body: JSON.stringify({
                     userId: event.target.userId.value,
-                    id: event.target.inputId.value,
                     title: event.target.title.value,
                     completed: event.target.completed.value
                 })
@@ -89,9 +57,9 @@ const populateUpdateForm = async (event) => {
 
     //fill out the form with the data from the comment we just got from the API
     document.querySelector("#userId").value = ID.userId;
-    document.querySelector("#inputId").value = ID.inputId;
     document.querySelector("#title").value = ID.title;
     document.querySelector("#completed").value = ID.completed;
+    document.querySelector("#id").value = ID.id
 
 }
 
